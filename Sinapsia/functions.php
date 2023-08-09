7c3740b1aa889d2a37550e9cfbe7365b4f8186b7
JanoPortnoi <47436853@est.ort.edu.ar>
2023-08-09 10:38:27 -0300

    feat:(funciones)
    Cree funciones para la conexion con mysql y para verificar si el email es valido.

--- Sinapsia/functions.php
+++ Sinapsia/functions.php
@@ -3,4 +3,21 @@ function post_request(){
     return ($_SERVER['REQUEST_METHOD']) === 'POST';
 
 }
+
+function Mysql($con){
+$mysqli = mysqli_init();
+$mysqli->ssl_set(NULL, NULL, "cacert.pem", NULL, NULL);
+$mysqli->real_connect($con["HOST"], $con["USERNAME"], $con["PASSWORD"], $con["DATABASE"]);
+if($mysqli->connect_errno)
+exit('Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+
+return $mysqli;
+
+
+}
+function checkmmail($mail){
+if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+    exit('El email no es valido');
+  }
+}
 ?>
