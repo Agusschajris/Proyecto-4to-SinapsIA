5016508981f1831eb8d265255abac0a42113e86f
JanoPortnoi <47436853@est.ort.edu.ar>
2023-10-03 13:22:32 -0300

    feat(gitignore)

---
+++ Sinapsia/login/login.js
@@ -0,0 +1,7 @@
+const informacion = document.querySelector('.informacion');
+const interrogationbtn = document.querySelector('.textbox');
+
+
+interrogationbtn.addEventListener('click', () =>{
+    informacion.classList.add('active-popup')
+});
