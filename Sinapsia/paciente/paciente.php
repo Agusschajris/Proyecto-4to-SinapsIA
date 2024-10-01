3a40e1eac9150e7fe29c4631286ae8349829edeb
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-10-01 19:27:59 -0300

    feat(paciente): pgsql

--- Sinapsia/paciente/paciente.php
+++ Sinapsia/paciente/paciente.php
@@ -1,83 +1,144 @@
 <?php
-    include_once("../configuracion/functions.php");
-    require_once("../configuracion/dbconfig.php");
-    session_start();
-    if($_SESSION['loggedin']==false || !isset($_SESSION['loggedin'])){
-        header("Location:http://localhost/Proyecto-4to-SinapsIA/Sinapsia/login/Iniciosesion.php");
+
+include_once "../configuracion/functions.php";
+require_once "../configuracion/dbconfig.php";
+session_start();
+if ($_SESSION["loggedin"] == false || !isset($_SESSION["loggedin"])) {
+    header(
+        "Location:http://localhost/Proyecto-4to-SinapsIA/Sinapsia/login/Iniciosesion.php"
+    );
+}
+
+$mail = $_SESSION["mail"];
+$errores = [];
+
+if (!post_request()) {
+    return;
+}
+
+if (
+    empty($_POST["nombre"]) ||
+    empty($_POST["apellido"]) ||
+    empty($_POST["mail"]) ||
+    empty($_POST["dni"])
+) {
+    $errores[] = "Debe completar todos los campos";
+}
+
+if (!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
+    $errores[] = "El mail ingresado no es válido.";
+}
+
+if (!is_numeric($_POST["telefono"])) {
+    $errores[] = "El teléfono ingresado no es válido.";
+}
+
+if (!is_numeric($_POST["edad"]) || $_POST["edad"] < 0 || $_POST["edad"] > 120) {
+    $errores[] = "La edad ingresada no es válida.";
+}
+
+if (
+    !is_numeric($_POST["dni"]) ||
+    $_POST["dni"] < 0 ||
+    $_POST["dni"] > 99999999
+) {
+    $errores[] = "El DNI ingresado no es válido.";
+}
+
+if (
+    !is_numeric($_POST["numeroHistClinica"]) ||
+    $_POST["numeroHistClinica"] < 0 ||
+    $_POST["numeroHistClinica"] > 99999999
+) {
+    $errores[] = "El número de historia clínica ingresado no es válido.";
+}
+
+if (
+    !empty($_POST["obrasocial"]) &&
+    !preg_match("/^[a-zA-Z ]*$/", $_POST["obrasocial"])
+) {
+    $errores[] = "La obra social ingresada no es válida.";
+}
+
+if (
+    !empty($_POST["nombre"]) &&
+    !preg_match("/^[a-zA-Z ]*$/", $_POST["nombre"])
+) {
+    $errores[] = "El nombre ingresado no es válido.";
+}
+
+if (
+    !empty($_POST["apellido"]) &&
+    !preg_match("/^[a-zA-Z ]*$/", $_POST["apellido"])
+) {
+    $errores[] = "El apellido ingresado no es válido.";
+}
+
+if (!empty($errores)) {
+    foreach ($errores as $error) {
+        echo $error . "<br>";
     }
-   
-    $mail = $_SESSION['mail'];
-    $errores = [];
+    return;
+}
 
-    if(post_request()){
-       
-        if (empty($_POST['nombre']) || empty($_POST['apellido']) || empty($_POST['mail']) || empty($_POST['dni'])) {
-            $errores[] = "Debe completar todos los campos";
-        }
-        if(!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)){
-            $errores[] = "El mail ingresado no es válido.";
-        }
-        if(!is_numeric($_POST['telefono'])){
-            $errores[] = "El teléfono ingresado no es válido.";
-        }
-        if(!is_numeric($_POST['edad']) || $_POST['edad']<0 || $_POST['edad']>120){
-            $errores[] = "La edad ingresada no es válida.";
-        }
-        if(!is_numeric($_POST['dni']) || $_POST['dni']<0 || $_POST['dni']>99999999){
-            $errores[] = "El DNI ingresado no es válido.";
-        }
-        if(!is_numeric($_POST['numeroHistClinica']) || $_POST['numeroHistClinica']<0 || $_POST['numeroHistClinica']>99999999){
-            $errores[] = "El número de historia clínica ingresado no es válido.";
-        }
-        if(!empty($_POST['obrasocial']) && !preg_match("/^[a-zA-Z ]*$/",$_POST['obrasocial'])){
-            $errores[] = "La obra social ingresada no es válida.";
-        }
-        if(!empty($_POST['nombre']) && !preg_match("/^[a-zA-Z ]*$/",$_POST['nombre'])){
-            $errores[] = "El nombre ingresado no es válido.";
-        }
-        if(!empty($_POST['apellido']) && !preg_match("/^[a-zA-Z ]*$/",$_POST['apellido'])){
-            $errores[] = "El apellido ingresado no es válido.";
-        }
-    
-        if(empty($errores)){
-        
-            $query = "SELECT * FROM paciente WHERE mail_medico = ?";
-            $stmt = $mysqli->prepare($query); 
-            $stmt->bind_param("s", $mail);
-            $stmt->execute();
-        
-            $result = $stmt->get_result();
-            $paciente = $result->fetch_assoc();
-        
-            if ($stmt->num_rows > 0) {
-                echo "Ya existe el usuario";
-            } else {
-     
-                $sql = "INSERT INTO paciente (nombre, apellido, mail, telefono, edad, dni, obrasocial, numeroHistClinica, mail_medico) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                $stmt = $mysqli->prepare($sql);
-                $stmt->bind_param("sssiiisis", $_POST['nombre'], $_POST['apellido'], $_POST['mail'], $_POST['telefono'], $_POST['edad'], $_POST['dni'], $_POST['obrasocial'], $_POST['numeroHistClinica'], $mail);
-        
-                if ($stmt->execute()) {
-                    echo "Paciente creado!";
-                    header("Location:../home/index.php");
-                } else {
-                    echo "Error: " . $sql . "<br>" . $mysqli->error;
+/*
+$query = "SELECT * FROM paciente WHERE mail_medico = ?";
+$stmt = $mysqli->prepare($query);
+$stmt->bind_param("s", $mail);
+$stmt->execute();
 
-                }
-            }
-        
-        }
-        else{
-            foreach($errores as $error){
-                echo $error."<br>";
-        }
-           
-        } 
-    }
-    
-        
+$result = $stmt->get_result();
+$paciente = $result->fetch_assoc();
+*/
 
-    
+($result = pg_select($pgsql, "paciente", ["mail_medico" => $mail])) or
+    die("Error en la consulta" . pg_last_error());
+
+if (pg_num_rows($result) > 0) //if ($stmt->num_rows > 0) {
+    echo "Ya existe el usuario";
+    return;
+}
+
+/*
+$sql =
+    "INSERT INTO paciente (nombre, apellido, mail, telefono, edad, dni, obrasocial, numeroHistClinica, mail_medico) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param(
+    "sssiiisis",
+    $_POST["nombre"],
+    $_POST["apellido"],
+    $_POST["mail"],
+    $_POST["telefono"],
+    $_POST["edad"],
+    $_POST["dni"],
+    $_POST["obrasocial"],
+    $_POST["numeroHistClinica"],
+    $mail
+);
+*/
+
+$result = pg_insert(
+    $pgsql,
+    "paciente",
+    [
+        "nombre" => $_POST["nombre"],
+        "apellido" => $_POST["apellido"],
+        "mail" => $_POST["mail"],
+        "telefono" => $_POST["telefono"],
+        "edad" => $_POST["edad"],
+        "dni" => $_POST["dni"],
+        "obrasocial" => $_POST["obrasocial"],
+        "numeroHistClinica" => $_POST["numeroHistClinica"],
+        "mail_medico" => $mail,
+    ]
+);
+
+if ($result) {
+    echo "Paciente creado!";
+    header("Location:../home/index.php");
+} else {
+    echo "Error: " . $sql . "<br>" . $mysqli->error;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -87,13 +148,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" type="text/css" href="estilo.css"> -->
     <link rel="stylesheet" href="nuevoPaciente.css">
-    <title>Document</title>  
+    <title>Document</title>
 
 </head>
 <body>
     <div class="contenedor">
         <a href="#"><img src="../logos/back.png" alt="Inicio" class="back" onclick="window.location.href = '../home/index.php'">
-        </a> 
+        </a>
 
         <div class="nuevoPaciente">
 
@@ -114,24 +175,24 @@
                     <label>APELIDO</label>
                     <input type="text" name="apellido" id="apellido">
                     </p>
-                   
+
                     <p>
                     <label>MAIL</label>
                     <input type="email" name="mail" id="mail">
                     </p>
-                    
+
 
                     <p>
                     <label>TELÉFONO</label>
                     <input type="text" name="telefono"id="telefono">
                     </p>
-                  
+
 
                     <P>
                     <label>EDAD</label>
                     <input type="text" name="edad" id="edad">
                     </P>
-                    
+
 
                     <P>
                     <label>DNI</label>
@@ -143,14 +204,14 @@
                     <label>OBRA SOCIAL</label>
                      <input type="text" name="obrasocial" id="obrasocial">
                     </P>
-                   
+
                     <P>
                     <label>NÚMERO DE HISTORIA CLÍNICA</label>
                      <input type="text" name="numeroHistClinica" id="numeroHistClinica">
                     </P>
-                    
+
                 </div>
-                    
+
                 <p class="block guardar-button">
                     <input type="submit" value="GUARDAR" class="guardar">
                     </p>
