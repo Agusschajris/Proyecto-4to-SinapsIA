a5225f108fb7b0383f33413524dc16b97f2753a5
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-10-01 19:33:29 -0300

    refactor(dbconfig)

--- Sinapsia/configuracion/dbconfig.php
+++ Sinapsia/configuracion/dbconfig.php
@@ -1,11 +1,18 @@
 <?php
 $_ENV = parse_ini_file(".env");
-$pgsql = pg_connect("host=".$_ENV["PGHOST"]." dbname=".$_ENV["PGDATABASE"]." user=".$_ENV["PGUSER"]." password=".$_ENV["PGPASSWORD"])
-    or die('No se ha podido conectar: ' . pg_last_error());
+($pgsql = pg_connect(
+    "host=" .
+        $_ENV["PGHOST"] .
+        " dbname=" .
+        $_ENV["PGDATABASE"] .
+        " user=" .
+        $_ENV["PGUSER"] .
+        " password=" .
+        $_ENV["PGPASSWORD"]
+)) or die("No se ha podido conectar: " . pg_last_error());
 //$mysqli = mysqli_init();
 //$mysqli->ssl_set(NULL, NULL, "..\configuracion\cacert.pem", NULL, NULL);
 //$mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["NAME"]);
 //if($mysqli->connect_errno)
 //exit('Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
-
 ?>
