74a8b31a0ee3f048487632e74f4c7a3c43bd4f75
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-10-01 19:33:46 -0300

    refactor(functions)

--- Sinapsia/configuracion/functions.php
+++ Sinapsia/configuracion/functions.php
@@ -1,16 +1,20 @@
 <?php
 
-function post_request() {
-  return ($_SERVER['REQUEST_METHOD']) === 'POST';
-} 
-
-function get_request() {
-  return ($_SERVER['REQUEST_METHOD']) === 'GET';
+function post_request()
+{
+    return $_SERVER["REQUEST_METHOD"] === "POST";
 }
 
-function obtener_cuenta($pgsql, $id) {
-  return pg_select($pgsql, 'medico', array('id' => $id)) or die ('Error: ' . pg_last_error());
-  /*
+function get_request()
+{
+    return $_SERVER["REQUEST_METHOD"] === "GET";
+}
+
+function obtener_cuenta($pgsql, $id)
+{
+    return pg_select($pgsql, "medico", ["id" => $id]) or
+        die("Error: " . pg_last_error());
+    /*
   if ($result = pg_select($pgsql, 'medico', array('id' => $id)))
     return $result;
   else
@@ -29,42 +33,52 @@ function obtener_cuenta($pgsql, $id) {
   */
 }
 
-
-
-function checkmail($mail) {
-  if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-    echo('El email no es valido');
-  }
+function checkmail($mail)
+{
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        echo "El email no es valido";
+    }
 }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  
-  return $data;
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+
+    return $data;
 }
 
-function login($mysqli,$query,$stringtocheck){
-  if ($stmt = $mysqli->prepare($query)) {
-    $stmt -> bind_param("s",$stringtocheck);
-    $stmt->execute();
-    $stmt->store_result();
-    return $stmt; 
-  }
+function login($mysqli, $query, $stringtocheck)
+{
+    if ($stmt = $mysqli->prepare($query)) {
+        $stmt->bind_param("s", $stringtocheck);
+        $stmt->execute();
+        $stmt->store_result();
+        return $stmt;
+    }
 }
 
-function modificar_cuenta($pgsql, $datos) {
-  if ($result = pg_update($pgsql, 'medico', array(
-    'nombre' => $datos[0],
-    'apellido' => $datos[1],
-    'hospital' => $datos[2],
-    'telefono' => $datos[3]
-  ), array('mail' => $datos[4])))
-    return $result;
-  else
-    die('Error: ' . pg_last_error());
-  /*
+function modificar_cuenta($pgsql, $datos)
+{
+    if (
+        $result = pg_update(
+            $pgsql,
+            "medico",
+            [
+                "nombre" => $datos[0],
+                "apellido" => $datos[1],
+                "hospital" => $datos[2],
+                "telefono" => $datos[3],
+            ],
+            ["mail" => $datos[4]]
+        )
+    ) {
+        return $result;
+    } else {
+        die("Error: " . pg_last_error());
+    }
+    /*
   $sql = "UPDATE medico SET nombre = ?, apellido = ?, hospital = ?, telefono = ? WHERE mail = ?";
   if ($stmt = $mysqli->prepare($sql)) {
     $stmt -> bind_param("sssss",$datos[0],$datos[1],$datos[2],$datos[3],$datos[4]);
@@ -75,25 +89,28 @@ function modificar_cuenta($pgsql, $datos) {
   */
 }
 
-function capitalizar($string){
-  $string = strtolower($string);
-  $string = ucwords($string);
-  return $string;
+function capitalizar($string)
+{
+    $string = strtolower($string);
+    $string = ucwords($string);
+    return $string;
 }
 
-function eliminar_cuenta($mysqli,$mail){
-  $sql = "DELETE FROM medico WHERE mail = ?";
-  if ($stmt = $mysqli->prepare($sql)) {
-    $stmt->bind_param("s",$mail);
-    $stmt->execute();
-    $stmt->store_result();
-    return $stmt;
-  }
+function eliminar_cuenta($mysqli, $mail)
+{
+    $sql = "DELETE FROM medico WHERE mail = ?";
+    if ($stmt = $mysqli->prepare($sql)) {
+        $stmt->bind_param("s", $mail);
+        $stmt->execute();
+        $stmt->store_result();
+        return $stmt;
+    }
 }
 
-function chequear($string) {
-  return isset($string) ? $string : "No hay datos";
-  /*
+function chequear($string)
+{
+    return isset($string) ? $string : "No hay datos";
+    /*
   if (isset($string)) {
     $parto = $string;
     return $parto;
