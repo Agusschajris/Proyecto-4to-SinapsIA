3b79faa1573ee77b17bf01c11bdcfef7adbf3684
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-10-01 20:44:33 -0300

    feat(signup): pgsql

--- Sinapsia/signup2/signup2.php
+++ Sinapsia/signup2/signup2.php
@@ -1,77 +1,115 @@
 <?php
-include_once("../configuracion/dbconfig.php");
-include_once("../configuracion/functions.php");
+include_once "../configuracion/dbconfig.php";
+include_once "../configuracion/functions.php";
 session_start();
 
-$nombre = $_SESSION['nombre'];
-$apellido = $_SESSION['apellido'];
-$institucion = $_SESSION['institucion'];
-$dni = $_SESSION['dni'];
+$nombre = $_SESSION["nombre"];
+$apellido = $_SESSION["apellido"];
+$institucion = $_SESSION["institucion"];
+$dni = $_SESSION["dni"];
 $errores = [];
 
-
-
-if(post_request()){
-    if(!isset($_POST['mail'],$_POST['contrasenia'],$_POST['contrasenia2'])){
-        $errores[] = "Completa el formulario";
-    }
-    if(empty($_POST['mail']) || empty($_POST['contrasenia']) || empty($_POST['contrasenia2'])){
-        $errores[] = "Debe completar todos los campos";
-    }
-    if(!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)){
-        $errores[] = "El mail ingresado no es válido.";
-    }
-    if($_POST['contrasenia'] != $_POST['contrasenia2']){
-        $errores[] = "Las contraseñas no coinciden.";
-    }
-    if(!empty($_POST['contrasenia']) && strlen($_POST['contrasenia']) < 8){
-        $errores[] = "La contraseña debe tener al menos 8 caracteres.";
-    }
-    if(!empty($_POST['contrasenia']) && !preg_match("#[0-9]+#",$_POST['contrasenia'])) {
-        $errores[] = "La contraseña debe tener al menos un número.";
-    }
-    if(!empty($_POST['contrasenia']) && !preg_match("#[A-Z]+#",$_POST['contrasenia'])) {
-        $errores[] = "La contraseña debe tener al menos una mayúscula.";
-    }
-    $contcrypt = password_hash($_POST["contrasenia"],PASSWORD_DEFAULT);
-    $mail = $_POST['mail'];
-    if(empty($errores)){
-    if($stmt= $mysqli->prepare("SELECT mail,contrasenia,nombre FROM medico WHERE mail = ?")){
-        $stmt->bind_param("s",$_POST['mail']);
-        $stmt->execute();
-        $stmt->store_result();
-        if($stmt->num_rows > 0){
-            echo "Ya existe el usuario";
-    
-        }else{
-           
-      $sql = "INSERT INTO medico (nombre,apellido,contrasenia,hospital,dni,mail) VALUES (?, ?, ?, ?, ?, ?)";
-      $crearusuario = $mysqli->prepare($sql);
-    $crearusuario -> bind_param("ssssis",$nombre,$apellido,$contcrypt,$institucion,$dni,$mail);
-
-      
-      if($crearusuario->execute()){
-    echo"Usuario creado!";
-    header("Location: ../home/index.php"); 
-    $_SESSION['loggedin'] = true;
-    $_SESSION['mail'] = $mail;
-         
-    } 
-    else {
-        echo "Error: " . $sql . "<br>" . $mysqli->error;
-    }
-        }
-    }
+if (!post_request()) {
+    return;
 }
-else{
-    foreach($errores as $error){
-        echo $error."<br>";
-    }
-    
 
-    
+if (!isset($_POST["mail"], $_POST["contrasenia"], $_POST["contrasenia2"])) {
+    $errores[] = "Completa el formulario";
 }
+if (
+    empty($_POST["mail"]) ||
+    empty($_POST["contrasenia"]) ||
+    empty($_POST["contrasenia2"])
+) {
+    $errores[] = "Debe completar todos los campos";
 }
+if (!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
+    $errores[] = "El mail ingresado no es válido.";
+}
+if ($_POST["contrasenia"] != $_POST["contrasenia2"]) {
+    $errores[] = "Las contraseñas no coinciden.";
+}
+if (!empty($_POST["contrasenia"]) && strlen($_POST["contrasenia"]) < 8) {
+    $errores[] = "La contraseña debe tener al menos 8 caracteres.";
+}
+if (
+    !empty($_POST["contrasenia"]) &&
+    !preg_match("#[0-9]+#", $_POST["contrasenia"])
+) {
+    $errores[] = "La contraseña debe tener al menos un número.";
+}
+if (
+    !empty($_POST["contrasenia"]) &&
+    !preg_match("#[A-Z]+#", $_POST["contrasenia"])
+) {
+    $errores[] = "La contraseña debe tener al menos una mayúscula.";
+}
+
+if (!empty($errores)) {
+    foreach ($errores as $error) {
+        echo $error . "<br>";
+    }
+    return;
+}
+
+$contcrypt = password_hash($_POST["contrasenia"], PASSWORD_DEFAULT);
+$mail = $_POST["mail"];
+
+/*
+if (
+    $stmt = $mysqli->prepare(
+        "SELECT mail,contrasenia,nombre FROM medico WHERE mail = ?"
+    )
+) {
+    $stmt->bind_param("s", $_POST["mail"]);
+    $stmt->execute();
+    $stmt->store_result();
+*/
+
+$result = pg_query_params(
+    $pgsql,
+    "SELECT mail,contrasenia,nombre FROM medico WHERE mail = $1",
+    [$_POST["mail"]]
+) or die("Error en la consulta SQL: " . pg_last_error());
+
+if (pg_num_rows($result) > 0) //($stmt->num_rows > 0) {
+    echo "Ya existe el usuario";
+    return;
+}
+
+/*
+$sql =
+    "INSERT INTO medico (nombre,apellido,contrasenia,hospital,dni,mail) VALUES (?, ?, ?, ?, ?, ?)";
+$crearusuario = $mysqli->prepare($sql);
+$crearusuario->bind_param(
+    "ssssis",
+    $nombre,
+    $apellido,
+    $contcrypt,
+    $institucion,
+    $dni,
+    $mail
+);
+*/
+
+$result = pg_insert($pgsql, "medico", [
+    "nombre" => $nombre,
+    "apellido" => $apellido,
+    "contrasenia" => $contcrypt,
+    "hospital" => $institucion,
+    "dni" => $dni,
+    "mail" => $mail,
+]);
+
+if ($result) { //($crearusuario->execute()) {
+    echo "Usuario creado!";
+    header("Location: ../home/index.php");
+    $_SESSION["loggedin"] = true;
+    $_SESSION["mail"] = $mail;
+} else {
+    echo "Error: " . $sql . "<br>" . pg_last_error();//$mysqli->error;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -82,7 +120,7 @@ else{
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" type="text/css" href="estilo.css"> -->
     <link rel="stylesheet" href="signup2.css">
-    <title>Document</title>  
+    <title>Document</title>
 
 </head>
 <body>
@@ -92,10 +130,10 @@ else{
                 SIGN UP
                 <br>
             </H1>
-            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Prompt:ital,wght@0,400;0,500;1,200&display=swap" 
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&family=Prompt:ital,wght@0,400;0,500;1,200&display=swap"
                 rel="stylesheet">
             <form class="formulario" method="POST">
-                
+
                 <br><br>
                 MAIL
                 <br>
@@ -114,9 +152,9 @@ else{
                <button class="guardar" type="submit">
                 GUARDAR
             </button>
-                
+
             </form>
-            
+
             <br><br>
                 <!-- errores acá -->
 
@@ -127,4 +165,3 @@ else{
 
 </body>
 </html>
-
